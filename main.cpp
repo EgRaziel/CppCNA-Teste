0b76c5d4c21b1c977b42ae3c9154f977f3028f9b
José Gabriel do Nascimento <gjose4587@gmail.com>
2026-01-22 20:25:18 -0300

    v0.2.0
    Operators: Essential Tools for C++ Programming:
    
    - Testes com floats, operadores (inclusive de incremento e decremento), parênteses e prioridades de operadores.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,72 @@
 
 using namespace std;
 
-int main(void) {
+//v0.2.0: 22/01/2026 - Operators: Essential Tools for C++ Programming
+/*int main(void) {
+	int i, j, k;
+	
+	cout << "Enter i: ";
+	cin >> i;
+	cout << "Enter j: ";
+	cin >> j;
+	
+  i += 2;
+  j -= i;
+  k = i / j;
+  k += k;
+  k--;
+  j = k % i;
+  k += k + i;
+  k += k / j;
+  k *= k * k;
+  k += i * j;
+	
+	cout << k << endl;
+  system("pause");
+	return 0;
+}*/
+/*int main(void) {
+	float pi = 3.14159265359;
+  float pi_squared = pi * pi;
+	float x,y;
+
+	cout << "Enter value for x: ";
+	cin >> x;
+
+  float x_squared = x * x;
+  float downcalc = x_squared + (0.5);
+  float downcalcminus = x_squared - (0.5);
+	y = (x_squared / (pi_squared * downcalc)) * (1 + (x_squared / (pi_squared * (downcalcminus * downcalcminus))));
+	
+	cout << "y = " << y << endl;
+
+  system("pause");
+	return 0;
+}*/
+/*int main(void) {
+  float v=2;
+
+  float result = (v + 1) * 2;
+  std::cout << "result:  " << result <<  "  expected result :  6" << std::endl;
+
+  result = (v + 1) * ((v + 2) * 2);
+  std::cout << "result: " << result <<  "  expected result : 24" << std::endl;
+
+  result = (v - 1) * (2 + (2 * 2));
+  std::cout << "result:  " << result <<  "  expected result :  6" << std::endl;
+
+  result = (v + v) * (v + v) * 2;
+  std::cout << "result: " << result <<  "  expected result : 32" << std::endl;
+
+  result = ((int)v % 2) * (v + 2 * 2);
+  std::cout << "result:  " << result <<  "  expected result :  0" << std::endl;
+
+  system("pause");
+  return 0;
+}*/
+
+// v0.1.0: 21/01/2026 - The Basic of Integers and Variables
+/*int main(void) {
   int segundos = 60;
   int result1 = 3 * segundos;
   int result2 = 6 * segundos;
@@ -12,4 +77,4 @@ int main(void) {
   cout << "Result: " << finalResult << endl;
   system("pause");
   return 0;
-}
+}*/
