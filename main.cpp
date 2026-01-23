38d4be7bb125e11b38d43ca0ac54182f43f66753
José Gabriel do Nascimento <gjose4587@gmail.com>
2026-01-23 13:34:01 -0300

    v0.3.0
    Making Simple Decisions: Intro to Flow Control in C++:
    
    - Uso de operadores de comparação e introdução ao desvio condicional if.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,70 @@
 #include <iostream>
-
 using namespace std;
+
+//v0.3.0: 23/01/2026 - Making Simple Decisions: Intro to Flow Control in C++
+/*int main(void) {
+  int num1, num2, num3, num4;
+
+  cout << "Enter the first octet: ";
+  cin >> num1;
+  cout << "Enter the second octet: ";
+  cin >> num2;
+  cout << "Enter the third octet: ";
+  cin >> num3;
+  cout << "Enter the fourth octet: ";
+  cin >> num4;
+  if ((num1 >=0 && num1 <=255) && (num2 >=0 && num2 <=255) && (num3 >=0 && num3 <=255) && (num4 >=0 && num4 <=255)) {
+    cout << "\nThe IP address is valid: " << num1 << "." << num2 << "." << num3 << "." << num4 << endl;
+  } else {
+    cout << "\nThe IP address is not valid." << endl;
+  }
+  system("pause");
+  return 0;
+}*/
+/*int main(void) {
+  int v1, v2;
+  float result1, result2, diference, difabs;
+  const float epsilon = 0.000001;
+
+  cout << "Enter the first value: ";
+  cin >> v1;
+  cout << "Enter the second value: ";
+  cin >> v2;
+
+  result1 = 1 / (float)v1;
+  result2 = 1 / (float)v2;
+  diference = result1 - result2;
+  
+  if (diference < 0) {
+    difabs = -diference;
+  } else {
+    difabs = diference;
+  }
+
+  if (difabs <= epsilon) {
+    cout << "Results are equal (by 0.000001 epsilon)." << endl;
+  } else {
+    cout << "Results are not equal (by 0.000001 epsilon)." << endl;
+  }
+  system("pause");
+  return 0;
+}*/
+/*int main(void) {
+	bool answer; 
+	int value;
+	
+	cout << "Enter a value: ";
+	cin >> value;
+	
+	if ((value >= 0 && value < 10) || ((value * 2) < 20 && (value - 2) > -2) || ((value - 1) > 1 && (value / 2) < 10) || value == 111) {
+    cout << "THAT'S TRUE :)" << endl;
+  } else {
+    cout << "THAT'S NOT TRUE :(" << endl;
+  }
+
+  system("pause");
+	return 0;
+}*/
 
 //v0.2.0: 22/01/2026 - Operators: Essential Tools for C++ Programming
 /*int main(void) {
